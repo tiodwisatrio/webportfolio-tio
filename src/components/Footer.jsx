722b8b2131eac6f700a3737177cd7228deb13fa0
tiodwisatrio <tiodwisatrio27@gmail.com>
2023-04-04 23:54:57 +0700

    change footer background

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-center bg-slate-800 py-4 flex-col gap-y-2">
+    <div className="flex items-center justify-center blue-gradient py-4 flex-col gap-y-2">
       <p className="text-white text-[12px] opacity-80">
         © 2023 Tio Dwi Satrio. All rights reserved.
       </p>
