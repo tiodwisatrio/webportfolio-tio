83953cef464cb37b02397b4a6d06d0e2520596a7
tiodwisatrio <tiodwisatrio27@gmail.com>
2023-04-04 23:48:40 +0700

    modify a simple footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="flex items-center justify-center bg-slate-800 py-4 flex-col gap-y-2">
+      <p className="text-white text-[12px] opacity-80">
+        © 2023 Tio Dwi Satrio. All rights reserved.
+      </p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
